4d236e6d529695fbae64899185ea13fe0b88f16c
betomoedano <betomoedano@outlook.com>
2025-02-06 16:13:55 -0600

    clean home screen

--- app/(index,map)/account.tsx
+++ app/(index,map)/account.tsx
@@ -4,64 +4,7 @@ import * as AC from "@bacons/apple-colors";
 import { Image, View } from "react-native";
 
 export default function Page() {
-  return (
-    <Form.List navigationTitle="Account">
-      <Form.Section>
-        <Form.HStack style={{ gap: 12 }}>
-          <Image
-            source={{ uri: "https://github.com/evanbacon.png" }}
-            style={{
-              aspectRatio: 1,
-              height: 48,
-              borderRadius: 999,
-            }}
-          />
-          <View style={{ gap: 4 }}>
-            <Form.Text style={Form.FormFont.default}>Evan's world</Form.Text>
-            <Form.Text style={Form.FormFont.caption}>Today</Form.Text>
-          </View>
-        </Form.HStack>
-        <Form.Link
-          href="/account"
-          hint="Baconator"
-          systemImage={{ name: "gamecontroller.fill", color: AC.systemPink }}
-        >
-          Game Center
-        </Form.Link>
-      </Form.Section>
-
-      <Form.Section>
-        <Form.Link href="/">Apps</Form.Link>
-        <Form.Link href="/">Subscriptions</Form.Link>
-        <Form.Link href="/">Purchase History</Form.Link>
-        <Form.Link href="/">Notifications</Form.Link>
-      </Form.Section>
-
-      <Form.Section>
-        <Form.Text style={{ color: AC.link }} onPress={() => {}}>
-          Redeem Gift Card or Code
-        </Form.Text>
-        <Form.Text style={{ color: AC.link }} onPress={() => {}}>
-          Send Gift Card by Email
-        </Form.Text>
-        <Form.Text style={{ color: AC.link }} onPress={() => {}}>
-          Add Money to Account
-        </Form.Text>
-      </Form.Section>
-
-      <Form.Section>
-        <Form.Link href="/">Personalized Recommendations</Form.Link>
-      </Form.Section>
-
-      <Form.Section title="Upcoming automatic updates">
-        <Form.Text hint="3">Update All</Form.Text>
-
-        <AppUpdate icon="https://github.com/expo.png" name="Expo Go" />
-        <AppUpdate icon="https://github.com/facebook.png" name="Facebook" />
-        <AppUpdate icon="https://github.com/apple.png" name="Apple" />
-      </Form.Section>
-    </Form.List>
-  );
+  return <></>;
 }
 
 function AppUpdate({ name, icon }: { name: string; icon: string }) {
--- app/(index,map)/map.tsx
+++ app/(index,map)/map.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import * as Form from "@/components/ui/Form";
 import Stack from "@/components/ui/Stack";
 import * as AC from "@bacons/apple-colors";
@@ -10,160 +11,204 @@ import Animated, {
   useScrollViewOffset,
 } from "react-native-reanimated";
 import { AppleMaps } from "expo-maps";
+import { locationList } from "../../LocationList.js";
 
 export default function Page() {
-  const ref = useAnimatedRef();
-  const scroll = useScrollViewOffset(ref);
-  const style = useAnimatedStyle(() => ({
-    transform: [
-      { translateY: interpolate(scroll.value, [-120, -70], [50, 0], "clamp") },
-    ],
-  }));
+  const [lastEvent, setLastEvent] = React.useState<string>("");
+  // const ref = useAnimatedRef();
+  // const scroll = useScrollViewOffset(ref);
+  // const style = useAnimatedStyle(() => ({
+  //   transform: [
+  //     { translateY: interpolate(scroll.value, [-120, -70], [50, 0], "clamp") },
+  //   ],
+  // }));
 
+  console.log(lastEvent);
+  const markers = locationList.map((location, index) => {
+    const stores = location.stores;
+    const colors = [
+      AC.systemRed,
+      AC.systemBlue,
+      AC.systemGreen,
+      AC.systemOrange,
+      AC.systemPurple,
+      AC.systemPink,
+    ];
+    const color = colors[index % colors.length];
+    return stores.map((store) => ({
+      coordinates: {
+        latitude: store.point[0],
+        longitude: store.point[1],
+      },
+      title: store.name,
+      systemImage: "cup.and.heat.waves.fill",
+      tintColor: color,
+    }));
+  });
   return (
-    <Form.List ref={ref} navigationTitle="Bottom Sheet" listStyle="grouped">
-      {process.env.EXPO_OS !== "web" && (
-        <Stack.Screen
-          options={{
-            headerLeft: () => (
-              <View
-                style={{
-                  overflow: "hidden",
-                  paddingBottom: 10,
-                  marginBottom: -10,
-                }}
-              >
-                <Animated.View style={style}>
-                  <Text
-                    style={{
-                      color: AC.label,
-                      fontWeight: "bold",
-                      fontSize: 20,
-                    }}
-                  >
-                    Bottom Sheet
-                  </Text>
-                </Animated.View>
-              </View>
-            ),
-            headerTitle() {
-              return <></>;
-            },
-          }}
-        />
-      )}
-
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: "",
+        }}
+      />
       <View style={{ flex: 1 }}>
         <AppleMaps.View
           style={{ width: "auto", height: "100%" }}
           cameraPosition={{
             coordinates: {
-              latitude: 51.509865,
-              longitude: -0.1275,
+              latitude: 49.19163,
+              longitude: -122.85056,
             },
-            zoom: 1,
+            zoom: 8,
+          }}
+          onMapClick={(e) => {
+            setLastEvent(
+              JSON.stringify({ type: "onMapClick", data: e }, null, 2)
+            );
+          }}
+          onCameraMove={(e) => {
+            setLastEvent(
+              JSON.stringify({ type: "onCameraMove", data: e }, null, 2)
+            );
+          }}
+          markers={markers.flat()}
+          onMarkerClick={(e) => {
+            console.log(e);
           }}
         />
       </View>
+    </>
+    // <Form.List ref={ref} navigationTitle="Bottom Sheet" listStyle="grouped">
+    //   {process.env.EXPO_OS !== "web" && (
+    //     <Stack.Screen
+    //       options={{
+    //         headerLeft: () => (
+    //           <View
+    //             style={{
+    //               overflow: "hidden",
+    //               paddingBottom: 10,
+    //               marginBottom: -10,
+    //             }}
+    //           >
+    //             <Animated.View style={style}>
+    //               <Text
+    //                 style={{
+    //                   color: AC.label,
+    //                   fontWeight: "bold",
+    //                   fontSize: 20,
+    //                 }}
+    //               >
+    //                 Bottom Sheet
+    //               </Text>
+    //             </Animated.View>
+    //           </View>
+    //         ),
+    //         headerTitle() {
+    //           return <></>;
+    //         },
+    //       }}
+    //     />
+    //   )}
 
-      <Form.Section
-        title="Vision"
-        footer={
-          <Text>
-            Help improve Search by allowing Apple to store the searches you
-            enter into Safari, Siri, and Spotlight in a way that is not linked
-            to you.{"\n\n"}Searches include lookups of general knowledge, and
-            requests to do things like play music and get directions.{"\n"}
-            <Link style={{ color: AC.link }} href="/two">
-              About Search & Privacy...
-            </Link>
-          </Text>
-        }
-      >
-        <Text>Default</Text>
-        <Form.Text hint="Right">Hint</Form.Text>
-        <Text
-          onPress={() => {
-            console.log("Hey");
-          }}
-        >
-          Pressable
-        </Text>
+    //   <Form.Section
+    //     title="Vision"
+    //     footer={
+    //       <Text>
+    //         Help improve Search by allowing Apple to store the searches you
+    //         enter into Safari, Siri, and Spotlight in a way that is not linked
+    //         to you.{"\n\n"}Searches include lookups of general knowledge, and
+    //         requests to do things like play music and get directions.{"\n"}
+    //         <Link style={{ color: AC.link }} href="/two">
+    //           About Search & Privacy...
+    //         </Link>
+    //       </Text>
+    //     }
+    //   >
+    //     <Text>Default</Text>
+    //     <Form.Text hint="Right">Hint</Form.Text>
+    //     <Text
+    //       onPress={() => {
+    //         console.log("Hey");
+    //       }}
+    //     >
+    //       Pressable
+    //     </Text>
 
-        <Text style={{ fontWeight: "bold", color: AC.systemPink }}>
-          Custom style
-        </Text>
-        <Form.Text bold>Bold</Form.Text>
+    //     <Text style={{ fontWeight: "bold", color: AC.systemPink }}>
+    //       Custom style
+    //     </Text>
+    //     <Form.Text bold>Bold</Form.Text>
 
-        <View>
-          <Text>Wrapped</Text>
-        </View>
+    //     <View>
+    //       <Text>Wrapped</Text>
+    //     </View>
 
-        {/* Table style: | A   B |*/}
-        <Form.HStack>
-          <Text style={Form.FormFont.default}>Foo</Text>
-          <View style={{ flex: 1 }} />
-          <Text style={Form.FormFont.secondary}>Bar</Text>
-        </Form.HStack>
-      </Form.Section>
-      <Form.Section title="Links">
-        {/* Table style: | A   B |*/}
-        <Link href="/two">Next</Link>
+    //     {/* Table style: | A   B |*/}
+    //     <Form.HStack>
+    //       <Text style={Form.FormFont.default}>Foo</Text>
+    //       <View style={{ flex: 1 }} />
+    //       <Text style={Form.FormFont.secondary}>Bar</Text>
+    //     </Form.HStack>
+    //   </Form.Section>
+    //   <Form.Section title="Links">
+    //     {/* Table style: | A   B |*/}
+    //     <Link href="/two">Next</Link>
 
-        <Form.Link target="_blank" href="https://evanbacon.dev">
-          Target _blank
-        </Form.Link>
+    //     <Form.Link target="_blank" href="https://evanbacon.dev">
+    //       Target _blank
+    //     </Form.Link>
 
-        <Link href="/two">
-          <View style={{ gap: 4 }}>
-            <Form.Text>Evan's iPhone</Form.Text>
-            <Text style={Form.FormFont.caption}>This iPhone 16 Pro Max</Text>
-          </View>
-        </Link>
+    //     <Link href="/two">
+    //       <View style={{ gap: 4 }}>
+    //         <Form.Text>Evan's iPhone</Form.Text>
+    //         <Text style={Form.FormFont.caption}>This iPhone 16 Pro Max</Text>
+    //       </View>
+    //     </Link>
 
-        <Link href="https://expo.dev">Expo</Link>
+    //     <Link href="https://expo.dev">Expo</Link>
 
-        <Form.Link href="/two" hint="Normal">
-          Hint + Link
-        </Form.Link>
-      </Form.Section>
+    //     <Form.Link href="/two" hint="Normal">
+    //       Hint + Link
+    //     </Form.Link>
+    //   </Form.Section>
 
-      <Form.Section title="Icons">
-        <Form.Link href="/two" systemImage="star">
-          Link + Icon
-        </Form.Link>
-        <Form.Link
-          href="/two"
-          systemImage={{ name: "car.fill", color: AC.systemPurple }}
-        >
-          Custom color in link
-        </Form.Link>
-        <Form.Text systemImage="airpodspro.chargingcase.wireless.fill">
-          Item
-        </Form.Text>
+    //   <Form.Section title="Icons">
+    //     <Form.Link href="/two" systemImage="star">
+    //       Link + Icon
+    //     </Form.Link>
+    //     <Form.Link
+    //       href="/two"
+    //       systemImage={{ name: "car.fill", color: AC.systemPurple }}
+    //     >
+    //       Custom color in link
+    //     </Form.Link>
+    //     <Form.Text systemImage="airpodspro.chargingcase.wireless.fill">
+    //       Item
+    //     </Form.Text>
 
-        <Form.Link
-          style={{
-            color: AC.systemGreen,
-          }}
-          href="/two"
-          systemImage="photo.on.rectangle"
-        >
-          Icon inherits link color
-        </Form.Link>
-      </Form.Section>
+    //     <Form.Link
+    //       style={{
+    //         color: AC.systemGreen,
+    //       }}
+    //       href="/two"
+    //       systemImage="photo.on.rectangle"
+    //     >
+    //       Icon inherits link color
+    //     </Form.Link>
+    //   </Form.Section>
 
-      <Form.Section title="Table">
-        {/* Table style: | A   B |*/}
-        <Form.Text hint="Expo Router v4">SDK 52</Form.Text>
+    //   <Form.Section title="Table">
+    //     {/* Table style: | A   B |*/}
+    //     <Form.Text hint="Expo Router v4">SDK 52</Form.Text>
 
-        {/* Custom version of same code */}
-        <Form.HStack>
-          <Text style={Form.FormFont.default}>SDK 51</Text>
-          <View style={{ flex: 1 }} />
-          <Text style={Form.FormFont.secondary}>Expo Router v3</Text>
-        </Form.HStack>
-      </Form.Section>
-    </Form.List>
+    //     {/* Custom version of same code */}
+    //     <Form.HStack>
+    //       <Text style={Form.FormFont.default}>SDK 51</Text>
+    //       <View style={{ flex: 1 }} />
+    //       <Text style={Form.FormFont.secondary}>Expo Router v3</Text>
+    //     </Form.HStack>
+    //   </Form.Section>
+    // </Form.List>
   );
 }
